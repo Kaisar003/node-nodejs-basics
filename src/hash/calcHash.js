59a230dc98d720616428ec2a1e272684685d4027
KaSeKe <kaisar.101003@gmail.com>
2025-04-24 19:47:09 +0500

    hash task!

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import fs from 'fs'
+import crypto from 'crypto'
+
 const calculateHash = async () => {
-    // Write your code here 
+    // Write your code here
+    const hash = crypto.createHash('sha256');
+    const fileStream = fs.createReadStream('files/fileToCalculateHashFor.txt')
+
+    fileStream.on('data', (chunk) => {
+        hash.update(chunk);
+    });
+
+    fileStream.on('end', () => {
+        const fileHash = hash.digest('hex');
+        console.log(fileHash);
+    });
+
+    fileStream.on('error', (err) => {
+        console.error('Error reading the file:', err);
+    });
 };
 
 await calculateHash();
