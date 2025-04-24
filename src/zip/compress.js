b8abf2e1fd79692fc4a232997dcbfcf5327c4cfb
KaSeKe <kaisar.101003@gmail.com>
2025-04-24 23:12:45 +0500

    zip tasks!

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,13 @@
+import zlib from 'zlib';
+import fs from 'fs';
+
 const compress = async () => {
     // Write your code here 
+    const gzip = zlib.createGzip();
+    const input = fs.createReadStream('files/fileToCompress.txt');
+    const output = fs.createWriteStream('files/archive.gz');
+
+    input.pipe(gzip).pipe(output);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,13 @@
+import zlib from 'zlib';
+import fs from 'fs';
+
 const decompress = async () => {
     // Write your code here 
+    const gunzip = zlib.createGunzip();
+    const input = fs.createReadStream('files/archive.gz');
+    const output = fs.createWriteStream('files/fileToCompress.txt');
+
+    input.pipe(gunzip).pipe(output);
 };
 
 await decompress();
