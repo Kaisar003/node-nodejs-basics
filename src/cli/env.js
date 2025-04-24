32a09d4905cca15e9649e30bd1f461ce683cab50
KaSeKe <kaisar.101003@gmail.com>
2025-04-24 18:53:08 +0500

    cli tasks!

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
+process.env.RSS_name1 = 'value1';
+process.env.RSS_name2 = 'value2';
+
 const parseEnv = () => {
     // Write your code here 
+    let result = '';
+    for (const key in process.env) {
+        if (key.startsWith('RSS_')) {
+            result += `${key}=${process.env[key]}; `;
+        }
+    }
+    console.log(result.trim());
 };
 
 parseEnv();
